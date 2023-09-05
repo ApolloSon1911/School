52febf81e03ab9fd41fd17ab9703c6aaf845db1b
S7916781 <S7916781@CFSTEC219-15>
2023-09-05 13:38:55 -0500

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+    // Step 1: Declare Variables
+
+    double number1, number2;
+
+
+
+    // Step 2: Ask the user to input two numbers
+
+    cout << "Enter the first number: ";
+
+    cin >> number1;
+
+
+
+    cout << "Enter the second number: ";
+
+    cin >> number2;
+
+
+
+    // Step 3: Perform arithmetic operations and display the results
+
+    cout << "The sum of the two numbers is: " << number1 + number2 << endl;
+
+        cout << "The difference between the two numbers is: " << number1 - number2 << endl;
+
+        cout << "The product of the two numbers is: " << number1 * number2 << endl;
+
+        cout << "The division result of the two numbers is:" << number1 / number2 << endl;
+
+
+
+        return 0;
+
+}
