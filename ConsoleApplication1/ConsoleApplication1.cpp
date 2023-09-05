d238d09098a4da8c00d81d572b3e73bcffc46be2
ApolloSon1911 <86748223+ApolloSon1911@users.noreply.github.com>
2023-09-05 15:25:11 -0500

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+double sum(double a, double b)
+{
+    return a + b;
+}
+double sub(double a, double b)
+{
+    return a - b;
+}
+double div(double a, double b)
+{
+    return a / b;
+}
+double pro(double a, double b)
+{
+    return a * b;
+}
+double prosum(double a, double b)
+{
+    return (a + b) * (a + b);
+}
 
 
 int main() {
@@ -28,14 +48,17 @@ int main() {
 
     // Step 3: Perform arithmetic operations and display the results
 
-    cout << "The sum of the two numbers is: " << number1 + number2 << endl;
+    cout << "The sum of the two numbers is: " << sum(number1, number2) << endl;
 
-        cout << "The difference between the two numbers is: " << number1 - number2 << endl;
+    cout << "The difference between the two numbers is: " << sub(number1, number2) << endl;
 
-        cout << "The product of the two numbers is: " << number1 * number2 << endl;
+    cout << "The product of the two numbers is: " << pro(number1, number2) << endl;
 
-        cout << "The division result of the two numbers is:" << number1 / number2 << endl;
+    cout << "The division result of the two numbers is:" << div(number1, number2) << endl;
 
+    cout << "The Product of the sums of the two numbers is:" << prosum(number1, number2) << endl;
+
+    cout << "The Product of the sums of the two numbers is:" << pro(sum(number1, number2), sum(number1, number2)) << endl;
 
 
         return 0;
